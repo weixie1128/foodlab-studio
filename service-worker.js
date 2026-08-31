21768cce334dbdc0cd560d97b0938c9a086c8f91
weixie1128 <2408475894@qq.com>
2026-08-31 20:08:28 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='foodlab-studio-v0.14.7';
-const ASSETS=['./','./index.html','./styles.css?v=0.14.7','./app.js?v=0.14.7','./chart-fixes.js?v=0.14.7','./template-fixes.js?v=0.14.7'];
+const CACHE='foodlab-studio-v0.14.8';
+const ASSETS=['./','./index.html','./styles.css?v=0.14.8','./app.js?v=0.14.8','./chart-fixes.js?v=0.14.8','./template-fixes.js?v=0.14.8'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
